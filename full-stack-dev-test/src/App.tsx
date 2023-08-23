e85da4b91f3b70eaa49f2c0c8019ca155d21e863
FnHafiz <farhanhafiz55@gmail.com>
2023-08-23 21:21:06 +0700

    feat: add homepage

--- full-stack-dev-test/src/App.tsx
+++ full-stack-dev-test/src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/landing-page";
+import { HomePage } from "./pages/home-page";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/*" element={<LandingPage />} />
+				<Route path="/home" element={<HomePage />} />
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ full-stack-dev-test/src/pages/home-page/index.tsx
@@ -0,0 +1,14 @@
+import { ToastContainer } from "react-toastify";
+import { useState } from "react";
+
+export const HomePage = () => {
+	return (
+		<div className="flex flex-col md:flex-row">
+			<div className="w-full h-screen bg-purple-600 flex flex-col justify-center px-20]">
+				<p className="w-full text-6xl font-bold text-center text-white pt-8 mb-8 mt">
+					Helllooo
+				</p>
+			</div>
+		</div>
+	);
+};
